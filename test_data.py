e8d451b7505b7a74ca624c2032bc41736437cbf3
Nadia Nahar <NadiaIT@users.noreply.github.com>
2022-02-22 16:54:25 -0500

    test assert fixed

--- test_data.py
+++ test_data.py
@@ -6,7 +6,7 @@ class TestLoadData(unittest.TestCase):
     def test_get_data(self):
         data = get_data()  # Act
 
-        assert(data == 1, "Data fetched correctly")
+        assert data == 1, "Data fetched correctly"
 
 if __name__ == '__main__':
     unittest.main()
