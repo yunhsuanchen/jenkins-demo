b30f4258ee1a34cb2955ebbdf324387d79199d8d
Luke Dramko <lukedram@andrew.cmu.edu>
2022-02-24 20:30:25 -0500

    Break the test by changing number to 3.

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def get_data():
 	# do some data fetch activity and store in a file
-	return 1
+	return 3
 
 def update_data():
 	# do a later data fetch activity and append in the prev file
