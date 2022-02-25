a3abb9ada35f4a3fcb38bbd30cb7b1eea5d77f8b
Luke Dramko <lukedram@andrew.cmu.edu>
2022-02-24 20:32:17 -0500

    Fix bug in data.py

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def get_data():
 	# do some data fetch activity and store in a file
-	return 3
+	return 1
 
 def update_data():
 	# do a later data fetch activity and append in the prev file
